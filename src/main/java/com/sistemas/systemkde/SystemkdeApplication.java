41cf1866c2ec6198242b0c12d50d0d2230f755c8
wagnerdsodre <wagnerdsodre@gmail.com>
2021-05-05 07:35:47 -0300

    Project created

---
+++ src/main/java/com/sistemas/systemkde/SystemkdeApplication.java
@@ -0,0 +1,13 @@
+package com.sistemas.systemkde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemkdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemkdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemas/systemkde/SystemkdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemas.systemkde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemkdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
