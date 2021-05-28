6cc6c93100928f55cfa578fcaccfa44d1449908a
wagnerdsodre <wagnerdsodre@gmail.com>
2021-05-28 16:52:47 -0300

    OrderStatus enum

--- src/main/java/com/sistemas/systemkde/config/testConfig.java
+++ src/main/java/com/sistemas/systemkde/config/testConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.sistemas.systemkde.entities.Order;
 import com.sistemas.systemkde.entities.User;
+import com.sistemas.systemkde.entities.enums.OrderStatus;
 import com.sistemas.systemkde.repositories.OrderRepository;
 import com.sistemas.systemkde.repositories.UserRepository;
 
@@ -35,9 +36,9 @@ public class testConfig implements CommandLineRunner {
 		User u5 = new User(null, "Paul kramp", "reddull@gmail.com", "977777777", "123456");
 		
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENTS, u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENTS, u1);
 		
 					
 		userRepository.saveAll(Arrays.asList(u1,u2,u3,u4,u5));
